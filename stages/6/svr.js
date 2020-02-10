59c5a2c61d2465bb0dda68458f1f5279ddca09e1
Jacek Kopecky <jacek@jacek.cz>
2020-02-10 18:59:34 +0000

    copy stage 5 into 6

---
+++ stages/6/svr.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// message board app
+// stage 5: refactor to separate http/web code from core logic
+const express = require('express');
+const app = express();
+const mb = require('./messageboard');
+
+app.use(express.static('client'));
+
+function getMessages(req, res) {
+  res.json(mb.getMessages());
+}
+
+function getMessage(req, res) {
+  const result = mb.getMessage(req.params.id);
+  if (!result) {
+    res.status(404).send('No match for that ID.');
+    return;
+  }
+  res.json(result);
+}
+
+function postMessage(req, res) {
+  const messages = mb.addMessage(req.body.msg);
+  res.json(messages);
+}
+
+app.get('/messages', getMessages);
+app.get('/messages/:id', getMessage);
+app.post('/messages', express.json(), postMessage);
+
+app.listen(8080);
