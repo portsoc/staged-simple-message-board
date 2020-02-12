2c5dae7c54e033a8821708c60c9132b760587612
Jacek Kopecky <jacek@jacek.cz>
2020-02-12 15:27:06 +0000

    stage 9 tweaks
    cleaner SQL,
    config for our VMs

--- stages/9/messageboard.js
+++ stages/9/messageboard.js
@@ -11,25 +11,25 @@ sql.on('error', (err) => {
 });
 
 async function listMessages() {
-  const q = 'SELECT * FROM messageboard ORDER BY time DESC LIMIT 10;';
+  const q = 'SELECT * FROM Messageboard ORDER BY time DESC LIMIT 10;';
   const result = await sql.query(q);
   return result.rows;
 }
 
 async function findMessage(id) {
-  const q = 'SELECT * FROM messageboard WHERE id = $1;';
+  const q = 'SELECT * FROM Messageboard WHERE id = $1;';
   const result = await sql.query(q, [id]);
   return result.rows[0];
 }
 
 async function addMessage(msg) {
-  const q = 'INSERT INTO messageboard (msg) VALUES ($1);';
+  const q = 'INSERT INTO Messageboard (msg) VALUES ($1);';
   await sql.query(q, [msg]);
   return listMessages();
 }
 
 async function editMessage(updatedMessage) {
-  const q = 'UPDATE messageboard SET msg = $1 WHERE id = $2;';
+  const q = 'UPDATE Messageboard SET msg = $1 WHERE id = $2;';
   await sql.query(q, [updatedMessage.msg, updatedMessage.id]);
   return findMessage(updatedMessage.id);
 }
