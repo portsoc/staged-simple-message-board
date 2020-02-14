7f97d2c5db2b94e62c8769a97aa9b980fead24da
Jacek Kopecky <jacek@jacek.cz>
2020-02-14 12:03:09 +0000

    stage 8

--- stages/svr.js
+++ stages/svr.js
@@ -1,19 +1,19 @@
 'use strict';
 
 // message board app
-// stage 7: add css
+// stage 8: Data belongs in a database
 const express = require('express');
 const app = express();
 const mb = require('./messageboard');
 
 app.use(express.static('client', { extensions: ['html'] }));
 
-function getMessages(req, res) {
-  res.json(mb.listMessages());
+async function getMessages(req, res) {
+  res.json(await mb.listMessages());
 }
 
-function getMessage(req, res) {
-  const result = mb.findMessage(req.params.id);
+async function getMessage(req, res) {
+  const result = await mb.findMessage(req.params.id);
   if (!result) {
     res.status(404).send('No match for that ID.');
     return;
@@ -21,19 +21,27 @@ function getMessage(req, res) {
   res.json(result);
 }
 
-function postMessage(req, res) {
-  const messages = mb.addMessage(req.body.msg);
+async function postMessage(req, res) {
+  const messages = await mb.addMessage(req.body.msg);
   res.json(messages);
 }
 
-function putMessage(req, res) {
-  const message = mb.editMessage(req.body);
+async function putMessage(req, res) {
+  const message = await mb.editMessage(req.body);
   res.json(message);
 }
 
-app.get('/messages', getMessages);
-app.get('/messages/:id', getMessage);
-app.put('/messages/:id', express.json(), putMessage);
-app.post('/messages', express.json(), postMessage);
+// wrap async function for express.js error handling
+function asyncWrap(f) {
+  return (req, res, next) => {
+    Promise.resolve(f(req, res, next))
+      .catch((e) => next(e || new Error()));
+  };
+}
+
+app.get('/messages', asyncWrap(getMessages));
+app.get('/messages/:id', asyncWrap(getMessage));
+app.put('/messages/:id', express.json(), asyncWrap(putMessage));
+app.post('/messages', express.json(), asyncWrap(postMessage));
 
 app.listen(8080);
