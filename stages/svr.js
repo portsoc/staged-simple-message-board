c5c2167d83074064040167ce7aada79dec7419f3
Jacek Kopecky <jacek@jacek.cz>
2020-02-14 12:03:09 +0000

    stage x

--- stages/svr.js
+++ stages/svr.js
@@ -1,47 +1,11 @@
-'use strict';
-
-// message board app
-// stage 9: PostgreSQL
 const express = require('express');
 const app = express();
-const mb = require('./messageboard');
+app.use('/', express.static('my_lovely_special_root_folder'));
 
-app.use(express.static('client', { extensions: ['html'] }));
+const messages = ['hello mum'];
 
-async function getMessages(req, res) {
-  res.json(await mb.listMessages());
-}
-
-async function getMessage(req, res) {
-  const result = await mb.findMessage(req.params.id);
-  if (!result) {
-    res.status(404).send('No match for that ID.');
-    return;
-  }
-  res.json(result);
-}
-
-async function postMessage(req, res) {
-  const messages = await mb.addMessage(req.body.msg);
+app.get('/messages', (req, res) => {
   res.json(messages);
-}
+});
 
-async function putMessage(req, res) {
-  const message = await mb.editMessage(req.body);
-  res.json(message);
-}
-
-// wrap async function for express.js error handling
-function asyncWrap(f) {
-  return (req, res, next) => {
-    Promise.resolve(f(req, res, next))
-      .catch((e) => next(e || new Error()));
-  };
-}
-
-app.get('/messages', asyncWrap(getMessages));
-app.get('/messages/:id', asyncWrap(getMessage));
-app.put('/messages/:id', express.json(), asyncWrap(putMessage));
-app.post('/messages', express.json(), asyncWrap(postMessage));
-
-app.listen(8080);
+app.listen(65535);
