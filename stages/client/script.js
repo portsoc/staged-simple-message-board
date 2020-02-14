a07933f01e1787bf131eb28a4ed08abd85d3f2b3
Jacek Kopecky <jacek@jacek.cz>
2020-02-14 12:03:09 +0000

    stage 2

--- stages/client/script.js
+++ stages/client/script.js
@@ -1,5 +1,35 @@
+/* Remove all child elements from a given element */
+function removeContentFrom(what) {
+  while (what.firstElementChild) {
+    what.firstElementChild.remove();
+  }
+}
+
+/* Add an array of messages to the page */
+function showMessages(messages, where) {
+  for (const message of messages) {
+    const li = document.createElement('li');
+    li.textContent = message;
+    where.append(li);
+  }
+}
+
+async function loadMessages() {
+  const response = await fetch('messages');
+  let messages;
+  if (response.ok) {
+    messages = await response.json();
+  } else {
+    messages = ['failed to load messages :-('];
+  }
+
+  const messagelist = document.querySelector('#messagelist');
+  removeContentFrom(messagelist);
+  showMessages(messages, messagelist);
+}
+
 function pageLoaded() {
-  console.log('js ready');
+  loadMessages();
 }
 
 window.addEventListener('load', pageLoaded);
--- stages/svr.js
+++ stages/svr.js
@@ -1,6 +1,17 @@
 // message board app
-// stage 1: basic web server
+// stage 2: add route for getting messages
 const express = require('express');
 const app = express();
 app.use(express.static('client'));
+
+const messages = [
+  'these are three default messages',
+  'delivered from the server',
+  'using a custom route',
+];
+
+app.get('/messages', (req, res) => {
+  res.json(messages);
+});
+
 app.listen(8080);
