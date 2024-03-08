07785c3d255f6666e77dcae4bd6e441e5a230094
Rich Boakes <rjb@port.ac.uk>
2024-03-08 16:00:53 +0000

    Remove Postgres

--- stages/9/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  // remove the next line if not running on our VM and connection to PostgreSQL doesn't work
-  host: '/var/run/postgresql',
-
-  database: 'messageboard',
-  statement_timeout: 5000,
-};
