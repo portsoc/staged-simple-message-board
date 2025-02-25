7affd9a34c26a77ad1be8a0fa196844372d4a29a
Rich Boakes <rjb@port.ac.uk>
2025-02-25 15:43:09 +0000

    stage 2

--- stages/client/index.js
+++ stages/client/index.js
@@ -1,5 +1,33 @@
+/* Remove all contents from a given element */
+function removeContentFrom(what) {
+  what.textContent = '';
+}
+
+/* Add an array of messages to the page */
+function showMessages(messages, where) {
+  for (const message of messages) {
+    const li = document.createElement('li');
+    li.textContent = message;
+    where.append(li);
+  }
+}
+
+async function loadMessages() {
+  const response = await fetch('messages');
+  let messages;
+  if (response.ok) {
+    messages = await response.json();
+  } else {
+    messages = ['failed to load messages :-('];
+  }
+
+  const messagelist = document.querySelector('#messagelist');
+  removeContentFrom(messagelist);
+  showMessages(messages, messagelist);
+}
+
 function pageLoaded() {
-  console.log('js ready');
+  loadMessages();
 }
 
 pageLoaded();
--- stages/svr.js
+++ stages/svr.js
@@ -1,7 +1,20 @@
 // message board app
-// stage 1: basic web server
+// stage 2: add route for getting messages
 import express from 'express';
 
 const app = express();
 app.use(express.static('client'));
+
+const messages = [
+  'these are three default messages',
+  'delivered from the server',
+  'using a custom route',
+];
+
+function getMessages(req, res) {
+  res.json(messages);
+}
+
+app.get('/messages', getMessages);
+
 app.listen(8080);
