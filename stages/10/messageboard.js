4091301e6511ee40f40f65c74203be823b4d7fbe
Matt Dennis <matthew.dennis@port.ac.uk>
2026-02-27 14:54:47 +0000

    AI thinks this will work

--- stages/10/messageboard.js
+++ stages/10/messageboard.js
@@ -1,11 +1,11 @@
 import { open } from 'sqlite';
-import sqlite3 from 'sqlite3';
+import Database from 'better-sqlite3';
 import uuid from 'uuid-random';
 
 async function init() {
   const db = await open({
     filename: './database.sqlite',
-    driver: sqlite3.Database,
+    driver: Database,
     verbose: true,
   });
   await db.migrate({ migrationsPath: './migrations-sqlite' });
--- stages/9/messageboard.js
+++ stages/9/messageboard.js
@@ -1,12 +1,12 @@
 import { open } from 'sqlite';
-import sqlite3 from 'sqlite3';
+import Database from 'better-sqlite3';
 import uuid from 'uuid-random';
 
 async function init() {
   const db = await open({
     filename: './database.sqlite',
-    driver: sqlite3.Database,
-    verbose: true
+    driver: Database,
+    verbose: true,
   });
   await db.migrate({ migrationsPath: './migrations-sqlite' });
   return db;
